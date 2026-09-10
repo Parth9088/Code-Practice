6a700eded6bc407e0bc11624ec74b03239def2bd
Parth Soni <parthsoni9088@gmail.com>
2026-09-10 15:24:13 +0530

    added new file

---
+++ num_Pos_neg_zero.js
@@ -0,0 +1,15 @@
+// Check whether a number is positive, negative, or zero.
+
+function checkNum(num) {
+    if (num == 0){
+        return "Zero";
+    } else if (num > 0){
+        return "Positive"; 
+    } else {
+        return "Negative";
+    }
+}
+
+console.log("Number is " + 5 + " → " + checkNum(5));
+console.log("Number is " + -3 + " → " + checkNum(-3));
+console.log("Number is " + 0 + " → " + checkNum(0));
