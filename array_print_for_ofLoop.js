1b6bdd7822932577a421b6bf945a25b9f59ecfa7
Parth Soni <parthsoni9088@gmail.com>
2026-09-16 16:13:41 +0530

    Print all elements of an array using for…of loop.

---
+++ array_print_for_ofLoop.js
@@ -0,0 +1,18 @@
+// Print all elements of an array using for…of loop.
+
+function printArrayElements(arr) {
+    for (const element of arr) {
+        console.log(element);
+    }       
+}
+
+printArrayElements([77,17,29,7,4,25]);
+
+// ---------------------------------------------------
+// Output:
+// 77
+// 17
+// 29
+// 7
+// 4
+// 25
